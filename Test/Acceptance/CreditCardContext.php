5a591563e53fafcc0457e365d2aff27fc53cbd09
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-03-12 14:09:54 -0300

    tests: create basic files for cc acceptance tests
    Define confs for credit card e22 tests.

---
+++ Test/Acceptance/CreditCardContext.php
@@ -0,0 +1,23 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class CreditCardContext extends RawMinkContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
