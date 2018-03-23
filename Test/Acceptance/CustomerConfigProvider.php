8242e85609e39e7078a107d651daf23a45f70dcd
Luiz Felipe (lflimeira) <lflimeira02@gmail.com>
2018-03-23 12:15:42 -0300

    fix: syntax error

--- Test/Acceptance/CustomerConfigProvider.php
+++ Test/Acceptance/CustomerConfigProvider.php
@@ -10,7 +10,7 @@ trait CustomerConfigProvider
         $customerAddressConfig = [
             'payment/mundipagg_customer_address/street_attribute' => 'street_1',
             'payment/mundipagg_customer_address/number_attribute' => 'street_2',
-            'payment/mundipagg_customer_address/district_attribute' => 'street_3'
+            'payment/mundipagg_customer_address/district_attribute' => 'street_3',
             'payment/mundipagg_customer_address/complement_attribute' => 'street_4',
         ];
 
