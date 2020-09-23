99ca5a41a44e8c2547aadcdd2121dfe2be2bdbb0
Wallace Ferreira <wallace.sf87@gmail.com>
2020-09-23 13:06:15 -0300

    ♻️ removes interface unnecessary

--- Model/Response/BankAccount.php
+++ Model/Response/BankAccount.php
@@ -4,6 +4,6 @@ namespace MundiPagg\MundiPagg\Model\Response;
 
 use MundiPagg\MundiPagg\Api\ObjectMapper\SplitRecipients\BankAccountMapperRequestInterface;
 
-class BankAccount extends \MundiPagg\MundiPagg\Model\Request\BankAccount implements BankAccountMapperRequestInterface
+class BankAccount extends \MundiPagg\MundiPagg\Model\Request\BankAccount
 {
 }
