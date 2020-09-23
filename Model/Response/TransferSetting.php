0210f14cc5cd63728c81e2d072e8d8b15900427d
Wallace Ferreira <wallace.sf87@gmail.com>
2020-09-23 13:07:27 -0300

    ♻️ removes interface unnecessary

--- Model/Response/TransferSetting.php
+++ Model/Response/TransferSetting.php
@@ -1,71 +1,7 @@
 <?php
 
-namespace MundiPagg\MundiPagg\Model\Request;
+namespace MundiPagg\MundiPagg\Model\Response;
 
-use MundiPagg\MundiPagg\Api\ObjectMapper\SplitRecipients\TransferSettingsMapperRequestInterface;
-
-class TransferSetting implements TransferSettingsMapperRequestInterface
+class TransferSetting extends \MundiPagg\MundiPagg\Model\Request\TransferSetting
 {
-    /**
-     * @var string
-     */
-    private $transferInterval;
-
-    /**
-     * @var bool
-     */
-    private $transferEnabled;
-
-    /**
-     * @var int
-     */
-    private $transferDay;
-
-    /**
-     * @return string
-     */
-    public function getTransferInterval()
-    {
-        return $this->transferInterval;
-    }
-
-    /**
-     * @param string $transferInterval
-     */
-    public function setTransferInterval($transferInterval)
-    {
-        $this->transferInterval = $transferInterval;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isTransferEnabled()
-    {
-        return $this->transferEnabled;
-    }
-
-    /**
-     * @param bool $transferEnabled
-     */
-    public function setTransferEnabled($transferEnabled)
-    {
-        $this->transferEnabled = $transferEnabled;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTransferDay()
-    {
-        return $this->transferDay;
-    }
-
-    /**
-     * @param int $transferDay
-     */
-    public function setTransferDay($transferDay)
-    {
-        $this->transferDay = $transferDay;
-    }
 }
